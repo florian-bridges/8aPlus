b5ebff622d5383b34639cb3b44a91a5818e8a1fd
florian <florianbridges97@gmail.com>
2023-10-06 21:24:21 +0200

    save models during training

--- src/train.py
+++ src/train.py
@@ -1,11 +1,18 @@
+import os
 import numpy as np
 import wandb
+import torch
 
 from src.test import test
 
+MODEL_OUTPUT_PATH = "output"
+
 def train(dataloader, model, loss_fn, optimizer, epochs=3, device="cuda"):
+
     size = len(dataloader.dataset)
     model.train()
+
+    max_val_acc = 0
 
     for epoch in range(epochs):
         print("Epoch: ", epoch)
@@ -28,19 +35,32 @@ def train(dataloader, model, loss_fn, optimizer, epochs=3, device="cuda"):
             optimizer.step()
             optimizer.zero_grad()
 
-            if batch % 100 == 0:
-                loss, current = loss.item(), (batch + 1) * len(X)
-                print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
         train_acc = train_acc.cpu().detach().numpy()/dp_count
-        train_loss = train_loss.cpu().detach().numpy()/dp_count
-        
+        train_loss = train_loss.cpu().detach().numpy()/dp_count        
         validation_loss, validation_acc = test(dataloader, model, loss_fn, device)
-        print(f"train acc:\t {train_acc}, train loss:\t {train_loss}, test acc:\t {validation_acc}, test loss:\t {validation_loss}")
-        wandb.log({
-            "train_accuracy": train_acc,
-            "train_loss": train_loss,
-            "validation_accuracy": validation_acc,
-            "validation_loss": validation_loss,
-            "epoch": epoch,
-        })
+        log_training_progress(train_acc, train_loss, validation_acc, validation_loss, epoch)
+        #log_model(f"model_epoch{epoch}.pt", model)
+        if  validation_acc > max_val_acc:
+            log_model(f"best_model.pt", model)
+            max_val_acc = validation_acc
+
+def create_dir(PATH):
+    if not os.path.exists(PATH):
+        os.makedirs(PATH)
+    
+
+def log_model(model_name, model):
+    create_dir(os.path.join(MODEL_OUTPUT_PATH, wandb.run.name))
+    torch.save(model.state_dict(), os.path.join(MODEL_OUTPUT_PATH, wandb.run.name, model_name))
+    
+
+def log_training_progress(train_acc, train_loss, validation_acc, validation_loss, epoch):
+    print(f"train acc:\t {train_acc}, train loss:\t {train_loss}, test acc:\t {validation_acc}, test loss:\t {validation_loss}")
+    wandb.log({
+        "train_accuracy": train_acc,
+        "train_loss": train_loss,
+        "validation_accuracy": validation_acc,
+        "validation_loss": validation_loss,
+        "epoch": epoch,
+    })
